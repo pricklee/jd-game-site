a191b92364f53edba2ab7e996be9140c9a773622
pricklee <karlonoob878@gmail.com>
2025-02-03 14:52:40 +0100

    seerbua

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ejs from 'ejs';
 const app = express();
 const port = 3000;
 
-app.set('views', './static');
+app.set('views', './views');
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 
