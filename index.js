d00b122d11894e2d4b311a00d1b1a95a371e74e4
pricklee <karlonoob878@gmail.com>
2025-02-03 14:46:03 +0100

    fsfsffdsfd

--- index.js
+++ index.js
@@ -5,33 +5,37 @@ import ejs from 'ejs';
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json({ extended: true }));
-
+app.set('views', './views');
 app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json({ extended: true }));
 app.use('/static', express.static('static'));
 
-app.get('/login', (req, res) => {
-	res.render('login.html');
+// Routes
+app.get('/', (req, res) => {
+    res.render('frontpage.html');
 });
 
-app.get('/', (req, res) => {
-	res.render('frontpage.html');
-})
+app.get('/login', (req, res) => {
+    res.render('login.html');
+});
 
 app.post('/login', express.json(), (req, res) => {
-	console.log(req.body);
-	res.redirect('/');
-});
-
-app.post('/register', express.json(), (req, res) => {
-	console.log(req.body);
-	res.redirect('/');
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.get('/register', (req, res) => {
-	res.render('register.html');
+    res.render('register.html');
 });
 
+app.post('/register', express.json(), (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+// Start Server
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
